491060d9407488fd390154e1083099cf58f54d76
Michael Bordner <mbordner@gmail.com>
2025-01-03 01:50:29 -0500

    day 02

--- day02/p1/main.go
+++ day02/p1/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2017/common/file"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 func main() {
 
+	sum := 0
+	lines, _ := file.GetLines("../data.txt")
+	for _, line := range lines {
+		tokens := strings.Fields(line)
+		vals := make([]int, len(tokens))
+		for i, t := range tokens {
+			vals[i], _ = strconv.Atoi(t)
+		}
+		sort.Ints(vals)
+		sum += vals[len(vals)-1] - vals[0]
+	}
+
+	fmt.Println(sum)
 }
--- day02/p2/main.go
+++ day02/p2/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2017/common/file"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	
+
+	sum := 0
+	lines, _ := file.GetLines("../data.txt")
+	for _, line := range lines {
+		tokens := strings.Fields(line)
+		vals := make([]int, len(tokens))
+		for i, t := range tokens {
+			vals[i], _ = strconv.Atoi(t)
+		}
+		sort.Ints(vals)
+		for j := len(vals) - 1; j >= 0; j-- {
+			for i := 0; i < j; i++ {
+				if vals[j]%vals[i] == 0 {
+					sum += vals[j] / vals[i]
+					break
+				}
+			}
+
+		}
+	}
+
+	fmt.Println(sum)
 }
