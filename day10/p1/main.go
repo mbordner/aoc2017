56c8a05bf565691a4d6fbec8028697cd8e4efc71
Michael Bordner <mbordner@gmail.com>
2025-01-07 11:30:11 -0500

    day 10

--- day10/p1/main.go
+++ day10/p1/main.go
@@ -13,27 +13,81 @@ type List struct {
 	ptr  int
 }
 
+func (l *List) Process(input []int) {
+	for i := 0; i < len(input); i++ {
+		if input[i] <= len(l.data) { // input must be valid
+			data := l.getData(input[i])
+			l.reverse(data)
+			l.setData(data)
+			l.advance(input[i] + l.skip)
+			l.skip++
+		}
+	}
+}
+
+func (l *List) advance(amt int) {
+	ptr := l.ptr + amt
+	for ptr >= len(l.data) {
+		ptr -= len(l.data)
+	}
+	l.ptr = ptr
+}
+
+func (l *List) getData(length int) []int {
+	data := make([]int, length)
+	for i, j := l.ptr, 0; j < length; i, j = i+1, j+1 {
+		if i >= len(l.data) {
+			for i >= len(l.data) {
+				i -= len(l.data)
+			}
+		}
+		data[j] = l.data[i]
+	}
+	return data
+}
+
+func (l *List) setData(data []int) {
+	for i, j := l.ptr, 0; j < len(data); i, j = i+1, j+1 {
+		if i >= len(l.data) {
+			for i >= len(l.data) {
+				i -= len(l.data)
+			}
+		}
+		l.data[i] = data[j]
+	}
+}
+
+func (l *List) reverse(data []int) {
+	for i, j, h := 0, len(data)-1, len(data)/2; i < h; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+}
+
 func main() {
 	args := []struct {
 		size  int
 		input string
 	}{
 		{size: 5, input: "../test.txt"},
+		{size: 256, input: "../data.txt"},
 	}
-	arg := args[0]
+	arg := args[1]
 
 	list := genList(arg.size)
 	input := getInputs(arg.input)
 
 	fmt.Println(list, input)
+	list.Process(input)
+	fmt.Println(list, input)
+	fmt.Println(list.data[0] * list.data[1])
 }
 
-func genList(size int) List {
+func genList(size int) *List {
 	list := List{data: make([]int, size)}
 	for i := 0; i < size; i++ {
 		list.data[i] = i
 	}
-	return list
+	return &list
 }
 
 func getInputs(filename string) []int {
